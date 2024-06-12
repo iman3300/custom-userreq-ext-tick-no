84740483359b5c16b6c74eebb334c743a971c4db
iman3300 <nuriman3300@gmail.com>
2024-06-12 11:19:49 +0800

    first try

--- module.custom-userreq-ext-tick-no.php
+++ module.custom-userreq-ext-tick-no.php
@@ -5,7 +5,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'custom-userreq-ext-tick-no/1.0.0',
+	'incident_cost_quotation_status_field/1.0.0',
 	array(
 		// Identification
 		//
